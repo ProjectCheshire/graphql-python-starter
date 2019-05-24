09107f31fecb7f622cb79afe5be0940d08fcec4a
Brendon Smith <br3ndonland@protonmail.com>
2019-05-23 22:24:17 -0400

    Autoformat Python files with Black
    We are using Black as our autoformatter:
    https://black.readthedocs.io/en/stable/
    
    Note that this commit was made with the --no-verify flag. The Python
    files are currently failing the flake8 and mypy hooks. These errors
    will be corrected in future commits.

--- models.py
+++ models.py
@@ -9,47 +9,50 @@ from flask_sqlalchemy import SQLAlchemy
 """
 
 # Many-to-Many: https://www.youtube.com/watch?v=OvhoYbjtiKc
-memberships = db.Table('memberships',
-    db.Column( 'hero_id', db.Integer, db.ForeignKey('heroes.id') ),
-    db.Column( 'faction_id', db.Integer, db.ForeignKey('factions.id') )
+memberships = db.Table(
+    "memberships",
+    db.Column("hero_id", db.Integer, db.ForeignKey("heroes.id")),
+    db.Column("faction_id", db.Integer, db.ForeignKey("factions.id")),
 )
 
 
 class Planet(db.Model):
-    __tablename__ = 'planets'
+    __tablename__ = "planets"
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String())
-    
-    heroes = db.relationship('Hero', backref='planets', lazy=True)
+
+    heroes = db.relationship("Hero", backref="planets", lazy=True)
 
     def __repr__(self):
-        return '<Planet %r>' % self.name
+        return "<Planet %r>" % self.name
 
 
 class Hero(db.Model):
-    __tablename__ = 'heroes'
+    __tablename__ = "heroes"
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String())
     race = db.Column(db.String())
-    
-    planet_id = db.Column(db.Integer, db.ForeignKey('planets.id'), nullable=True)
+
+    planet_id = db.Column(
+        db.Integer, db.ForeignKey("planets.id"), nullable=True
+    )
     memberships = db.relationship(
-        'Faction', 
-        secondary = memberships, 
-        backref = db.backref( 'memberships', lazy = 'dynamic' )
+        "Faction",
+        secondary=memberships,
+        backref=db.backref("memberships", lazy="dynamic"),
     )
 
     def __repr__(self):
-        return '<Hero %r>' % self.name
+        return "<Hero %r>" % self.name
 
 
 class Faction(db.Model):
-    __tablename__ = 'factions'
+    __tablename__ = "factions"
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String())
 
     def __repr__(self):
-        return '<Faction %r>' % self.name
+        return "<Faction %r>" % self.name
--- seed_db.py
+++ seed_db.py
@@ -8,19 +8,19 @@ db.create_all()
 
 print("\nCreating model entities with foreign keys...\n")
 
-f1 = Faction(name='Light Side')
-f2 = Faction(name='Dark Side')
+f1 = Faction(name="Light Side")
+f2 = Faction(name="Dark Side")
 
-p1 = Planet(name='Tatooine')
-p2 = Planet(name='Naboo')
-p3 = Planet(name='Moraband')
+p1 = Planet(name="Tatooine")
+p2 = Planet(name="Naboo")
+p3 = Planet(name="Moraband")
 
-h1 = Hero(name='Luke Skywalker', race='human', planet_id=1)
-h2 = Hero(name='Obi-Wan Kenobi', race='human', planet_id=1)
-h3 = Hero(name='Sheev Palpatine', race='human', planet_id=2)
-h4 = Hero(name='Darth Bane', race='human', planet_id=3)
+h1 = Hero(name="Luke Skywalker", race="human", planet_id=1)
+h2 = Hero(name="Obi-Wan Kenobi", race="human", planet_id=1)
+h3 = Hero(name="Sheev Palpatine", race="human", planet_id=2)
+h4 = Hero(name="Darth Bane", race="human", planet_id=3)
 
-new_entities = (f1, f2, p1, p2, p3, h1, h2, h3, h4,)
+new_entities = (f1, f2, p1, p2, p3, h1, h2, h3, h4)
 create_records = [(db.session.add(e)) for e in new_entities]
 breakpoint
 try:
@@ -30,7 +30,7 @@ except:
     print("\nError, could not create model objects.\n")
 else:
     for e in new_entities:
-        print(u'\N{check mark}' + " -- %s" % (e.name) )
+        print(u"\N{check mark}" + " -- %s" % (e.name))
 
 print("\n\nCreating faction memberships...\n")
 
@@ -43,10 +43,18 @@ db.session.commit()
 
 heroes = Hero.query.all()
 for h in heroes:
-    faction_names =[]
+    faction_names = []
     faction_loop = [faction_names.append(f.name) for f in h.memberships]
     faction_loop
-    print( u'\N{check mark}' + " -- " + h.name + " -- " + h.planets.name + " -- " + ", ".join(faction_names) )
+    print(
+        u"\N{check mark}"
+        + " -- "
+        + h.name
+        + " -- "
+        + h.planets.name
+        + " -- "
+        + ", ".join(faction_names)
+    )
 
 print("\nDONE.")
 print("\n===================================")
