b19ebb8de015087086e1e41f9b182ee5f01a224f
Henelllis <Hellis1001@gmail.com>
2019-07-04 09:19:30 -0400

    add more potential quries, ones with params

--- schema.py
+++ schema.py
@@ -9,7 +9,7 @@ from models import Planet as PlanetModel
 class Hero(SQLAlchemyObjectType):
     class Meta:
         model = HeroModel
-        only_fields = ("name", "race")
+        # only_fields = ("name", "race")
         interfaces = (relay.Node,)
 
 
@@ -27,13 +27,34 @@ class Planet(SQLAlchemyObjectType):
 
 class Query(graphene.ObjectType):
     # node = relay.Node.Field()
+    # Get Method :: Return an instance based on the given primary key identifier, or None if not found.
+    # 
     heroes = graphene.List(Hero)
+    factions = graphene.List(Faction)
+    hero = graphene.Field(Hero, id=graphene.String())
+    hero_name = graphene.Field(heroes, name=graphene.String())
+    find_race = graphene.Field(Hero, race=graphene.String() )
+
 
     def resolve_heroes(self, info):
         query = Hero.get_query(info)
         return query.all()
+
+    def resolve_planets(self, info):
+        query = Hero.get_query(info)
+        return query.all()
+
+    def resolve_hero(self, info,id):
+        query = Hero.get_query(info)
+        return query.get(id)
+
+    def resolve_hero_name(self, info, name):
+        query = Hero.get_query(info)
+        return query.filter_by(name=name)
+
+
     # all_heroes = SQLAlchemyConnectionField( Hero )
-    all_factions = SQLAlchemyConnectionField(Faction)
+    # all_factions = SQLAlchemyConnectionField(Faction)
     # all_planets = SQLAlchemyConnectionField( Planet )
 schema = graphene.Schema(query=Query)
 
